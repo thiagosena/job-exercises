de80ea0338b7a2e29116fd6f450565d40fee1498
Thiago Sena <lord.sena@gmail.com>
2022-08-03 16:59:18 -0300

    test(exercise1): adjust exception tests

--- exercise1/src/test/java/com/thiagosena/exercise1/CounterServiceTest.java
+++ exercise1/src/test/java/com/thiagosena/exercise1/CounterServiceTest.java
@@ -1,5 +1,6 @@
 package com.thiagosena.exercise1;
 
+import com.thiagosena.exercise1.exceptions.ValidateArgsException;
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 
@@ -7,22 +8,22 @@ public class CounterServiceTest {
 
     @Test
     void givenInvalidArgsLengthShouldThrowsRuntimeException() {
-        Assertions.assertThrows(RuntimeException.class, () -> CounterService.main(new String[]{"1"}));
+        Assertions.assertThrows(ValidateArgsException.class, () -> CounterService.main(new String[]{"1"}));
     }
 
     @Test
     void givenInvalidFirstArgsShouldThrowsRuntimeException() {
-        Assertions.assertThrows(RuntimeException.class, () -> CounterService.main(new String[]{"asd", "10"}));
+        Assertions.assertThrows(ValidateArgsException.class, () -> CounterService.main(new String[]{"asd", "10"}));
     }
 
     @Test
     void givenInvalidSecondArgsShouldThrowsRuntimeException() {
-        Assertions.assertThrows(RuntimeException.class, () -> CounterService.main(new String[]{"1", "-10"}));
+        Assertions.assertThrows(ValidateArgsException.class, () -> CounterService.main(new String[]{"1", "-10"}));
     }
 
     @Test
     void givenFirstNumberGreaterThanSecondShouldThrowsRuntimeException() {
-        Assertions.assertThrows(RuntimeException.class, () -> CounterService.main(new String[]{"10", "1"}));
+        Assertions.assertThrows(ValidateArgsException.class, () -> CounterService.main(new String[]{"10", "1"}));
     }
 
     @Test
